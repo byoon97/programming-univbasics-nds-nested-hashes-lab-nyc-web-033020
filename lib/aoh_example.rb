b76f3e7010ecb6ec2e229b9cc10e469740c4917d
Brandon Yoon <byoon5397@gmail.com>
2020-03-23 20:53:37 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,7 +11,7 @@ def assembled_aoh
 end
 
 def literal_aoh
-  newHash = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G].sort
+  newHash = [DON_G.sort, JOELLE_VD.sort, PAT_M.sort, KATE_G.sort, BRUCE_G.sort]
 end
 
 def aoh_lookup(aoh, row, key)
